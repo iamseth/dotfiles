65cf7992c947066b0bc94d2b39a1f0f41c253550
Seth Miller <seth@sethmiller>
2024-12-12 11:26:25 -0600

    update vim go

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -86,7 +86,20 @@ require("lazy").setup({
       end
     },
 
-    { "ray-x/go.nvim", ft = "go" },
+    { "ray-x/go.nvim", 
+      ft = {"go", 'gomod'},
+      event = {"CmdlineEnter"},
+      build = ':lua require("go.install").update_all_sync()',
+      config = function()
+        require("go").setup({
+          run_in_floaterm = { enable = true },
+        })
+
+      end
+    },
+
+    { "ray-x/guihua.lua" },
+      
 
     { "stevearc/oil.nvim",
       config = function()
