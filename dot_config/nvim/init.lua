3c958581d914f6b6ae5aa0ee375f6b3ed9a29d72
Seth Miller <seth.a.miller@coinbase.com>
2025-04-19 16:41:22 -0500

    update

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,139 +1,139 @@
-vim.opt.tabstop = 2                                                             -- number of spaces that a <Tab> in the file counts for
-vim.opt.shiftwidth = 2                                                          -- number of spaces to use for each step of (auto)indent
-vim.opt.expandtab = true                                                        -- convert tabs to spaces
-vim.opt.scrolloff = 8                                                           -- minimal number of screen lines to keep above and below the cursor
-vim.opt.number = true                                                           -- set numbered lines
-vim.opt.relativenumber = true                                                   -- set relative numbered lines
-vim.opt.pumheight = 10                                                          -- pop up menu heigt
-vim.opt.mouse = "anicr"                                                         -- allow the mouse to be used in neovim
-vim.opt.showmode = false                                                        -- we don't need to see things like -- INSERT -- anymore
-vim.opt.ignorecase = true                                                       -- ignore case in search patterns
-vim.opt.smartcase = true                                                        -- smart case
-vim.opt.splitright = true                                                       -- force all vertical splits to go to the right of current window
-vim.opt.splitbelow = true                                                       -- force all horizontal splits to go below current window
-vim.opt.termguicolors = true                                                    -- Enables 24-bit RGB color in the TUI. This is supported by most terminals.
-vim.opt.iskeyword:append('-', '_', '@')                                         -- treat dash, underscore and @ as part of a word
-vim.opt.clipboard = "unnamedplus"                                               -- allows neovim to access the system clipboard
-vim.opt.wrap = false                                                            -- display lines as one long line 
-vim.g.mapleader = ' '                                                           -- set the leader key to space
-vim.cmd("highlight WinSeparator guifg=#A678D3")                                 -- set the color of the window separator
-vim.o.guifont = "Source Code Pro:h11"                                           -- set the font for neovide
+vim.opt.tabstop = 2 -- number of spaces that a <Tab> in the file counts for
+vim.opt.shiftwidth = 2 -- number of spaces to use for each step of (auto)indent
+vim.opt.expandtab = true -- convert tabs to spaces
+vim.opt.scrolloff = 8 -- minimal number of screen lines to keep above and below the cursor
+vim.opt.number = true -- set numbered lines
+vim.opt.relativenumber = true -- set relative numbered lines
+vim.opt.pumheight = 10 -- pop up menu heigt
+vim.opt.mouse = "anicr" -- allow the mouse to be used in neovim
+vim.opt.showmode = false -- we don't need to see things like -- INSERT -- anymore
+vim.opt.ignorecase = true -- ignore case in search patterns
+vim.opt.smartcase = true -- smart case
+vim.opt.splitright = true -- force all vertical splits to go to the right of current window
+vim.opt.splitbelow = true -- force all horizontal splits to go below current window
+vim.opt.termguicolors = true -- Enables 24-bit RGB color in the TUI. This is supported by most terminals.
+vim.opt.iskeyword:append("-", "_", "@") -- treat dash, underscore and @ as part of a word
+vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
+vim.opt.wrap = false -- display lines as one long line
+vim.g.mapleader = " " -- set the leader key to space
+vim.cmd("highlight WinSeparator guifg=#A678D3") -- set the color of the window separator
+-- vim.o.guifont = "Source Code Pro:h18"                                           -- set the font for neovide
 
-require('plugins')
+require("plugins")
 
-local telescope = require('telescope.builtin')
+local telescope = require("telescope.builtin")
 local opts = { noremap = true, silent = true }
 
-vim.keymap.set('n', 'qq', ':q<CR>', opts)                                       -- quit
-vim.keymap.set('n', '-', ':Oil<CR>', opts)                                      -- open oil
-vim.keymap.set('n', 'J', '<C-w>j', opts)                                        -- move to window below
-vim.keymap.set('n', 'K', '<C-w>k', opts)                                        -- move to window above
-vim.keymap.set('n', 'H', '<C-w>h', opts)                                        -- move to window left
-vim.keymap.set('n', 'L', '<C-w>l', opts)                                        -- move to window right
-vim.keymap.set('n', 'ss', telescope.live_grep, opts)                            -- search in current directory
-vim.keymap.set('n', 'sl', telescope.buffers, opts)                              -- list open buffers 
-vim.keymap.set('n', 'ff', telescope.find_files, opts)                           -- find files using telescope
-vim.keymap.set('n', 'gd', ':lua vim.lsp.buf.definition()<CR>', opts)            -- go to definition
-vim.keymap.set('n', '<leader>v', ':vsplit<CR>', opts)                           -- vertical split
-vim.keymap.set('n', '<leader>h', ':split<CR>', opts)                            -- horizontal split
-vim.keymap.set('n', '<leader>r', ':ReloadConfig<CR>', opts)                     -- reload configuration
-vim.keymap.set('n', '<leader>w', ':SaveAll<CR>', opts)                          -- save all buffers
-vim.keymap.set('n', '<leader>g', ":LazyGit<CR>", opts)                          -- open lazygit
-vim.keymap.set('n', '<leader>t', ':Floaterminal<CR>', opts)                     -- open terminal
-vim.keymap.set('t', 'qq', '<C-\\><C-N>:q<CR>', opts)                            -- quit terminal
+vim.keymap.set("n", "qq", ":q<CR>", opts) -- quit
+vim.keymap.set("n", "-", ":Oil<CR>", opts) -- open oil
+vim.keymap.set("n", "J", "<C-w>j", opts) -- move to window below
+vim.keymap.set("n", "K", "<C-w>k", opts) -- move to window above
+vim.keymap.set("n", "H", "<C-w>h", opts) -- move to window left
+vim.keymap.set("n", "L", "<C-w>l", opts) -- move to window right
+vim.keymap.set("n", "ss", telescope.live_grep, opts) -- search in current directory
+vim.keymap.set("n", "sl", telescope.buffers, opts) -- list open buffers
+vim.keymap.set("n", "ff", telescope.find_files, opts) -- find files using telescope
+vim.keymap.set("n", "gd", ":lua vim.lsp.buf.definition()<CR>", opts) -- go to definition
+vim.keymap.set("n", "<leader>v", ":vsplit<CR>", opts) -- vertical split
+vim.keymap.set("n", "<leader>h", ":split<CR>", opts) -- horizontal split
+vim.keymap.set("n", "<leader>r", ":ReloadConfig<CR>", opts) -- reload configuration
+vim.keymap.set("n", "<leader>w", ":SaveAll<CR>", opts) -- save all buffers
+vim.keymap.set("n", "<leader>g", ":LazyGit<CR>", opts) -- open lazygit
+vim.keymap.set("n", "<leader>t", ":Floaterminal<CR>", opts) -- open terminal
+vim.keymap.set("t", "qq", "<C-\\><C-N>:q<CR>", opts) -- quit terminal
+vim.keymap.set("n", "<C-d>", "<C-d>zz", opts) -- center screen when scrolling down
+vim.keymap.set("n", "<C-u>", "<C-u>zz", opts) -- center screen when scrolling up
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-    local params = vim.lsp.util.make_range_params()
-    params.context = {only = {"source.organizeImports"}}
-    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
-    for cid, res in pairs(result or {}) do
-      for _, r in pairs(res.result or {}) do
-        if r.edit then
-          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-          vim.lsp.util.apply_workspace_edit(r.edit, enc)
-        end
-      end
-    end
-    vim.lsp.buf.format({async = false})
-  end
-})
+vim.api.nvim_create_user_command("SaveAll", function()
+	vim.cmd("wa!")
+	vim.notify("All buffers saved.", vim.log.levels.INFO, { title = "Buffers Written" })
+end, {})
+
+vim.api.nvim_create_user_command("ReloadConfig", function()
+	vim.cmd("source ~/.config/nvim/init.lua")
+	vim.notify("Configuration reloaded.", vim.log.levels.INFO, { title = "Config Reloaded" })
+end, {})
+
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   pattern = "*.go",
+--   callback = function()
+--     local params = vim.lsp.util.make_range_params()
+--     params.context = {only = {"source.organizeImports"}}
+--     local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+--     for cid, res in pairs(result or {}) do
+--       for _, r in pairs(res.result or {}) do
+--         if r.edit then
+--           local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+--           vim.lsp.util.apply_workspace_edit(r.edit, enc)
+--         end
+--       end
+--     end
+--     vim.lsp.buf.format({async = false})
+--   end
+-- })
 
 local state = {
-  floating = {
-    buf = -1,
-    win = -1,
-  },
-  zen_mode = false
+	floating = {
+		buf = -1,
+		win = -1,
+	},
+	zen_mode = false,
 }
 
-vim.api.nvim_create_user_command('ZenToggle', function()
-  local lualine = require('lualine').hide()
-  if state.zen_mode then
-    vim.cmd('Goyo!')
-    vim.cmd('Limelight!')
-    vim.cmd('PencilToggle')
-    state.zen_mode = false
-  else
-    vim.cmd('Goyo')
-    vim.cmd('Limelight')
-    vim.cmd('PencilToggle')
-    state.zen_mode = true
-  end
+vim.api.nvim_create_user_command("ZenToggle", function()
+	local lualine = require("lualine").hide()
+	if state.zen_mode then
+		vim.cmd("Goyo!")
+		vim.cmd("Limelight!")
+		vim.cmd("PencilToggle")
+		state.zen_mode = false
+	else
+		vim.cmd("Goyo")
+		vim.cmd("Limelight")
+		vim.cmd("PencilToggle")
+		state.zen_mode = true
+	end
 end, {})
 
 local function create_floating_window(opts)
-  opts = opts or {}
-  local width = opts.width or math.floor(vim.o.columns * 0.8)
-  local height = opts.height or math.floor(vim.o.lines * 0.8)
+	opts = opts or {}
+	local width = opts.width or math.floor(vim.o.columns * 0.8)
+	local height = opts.height or math.floor(vim.o.lines * 0.8)
 
-  local col = math.floor((vim.o.columns - width) / 2)
-  local row = math.floor((vim.o.lines - height) / 2)
+	local col = math.floor((vim.o.columns - width) / 2)
+	local row = math.floor((vim.o.lines - height) / 2)
 
-  local buf = nil
-  if vim.api.nvim_buf_is_valid(opts.buf) then
-    buf = opts.buf
-    vim.cmd.startinsert()
-  else
-    buf = vim.api.nvim_create_buf(false, true) -- No file, scratch buffer
-  end
+	local buf = nil
+	if vim.api.nvim_buf_is_valid(opts.buf) then
+		buf = opts.buf
+		vim.cmd.startinsert()
+	else
+		buf = vim.api.nvim_create_buf(false, true) -- No file, scratch buffer
+	end
 
-  local win_config = {
-    relative = "editor",
-    width = width,
-    height = height,
-    col = col,
-    row = row,
-    style = "minimal", 
-    border = "rounded",
-  }
+	local win_config = {
+		relative = "editor",
+		width = width,
+		height = height,
+		col = col,
+		row = row,
+		style = "minimal",
+		border = "rounded",
+	}
 
-  local win = vim.api.nvim_open_win(buf, true, win_config)
+	local win = vim.api.nvim_open_win(buf, true, win_config)
 
-  return { buf = buf, win = win }
+	return { buf = buf, win = win }
 end
 
-vim.api.nvim_create_user_command('Floaterminal', function()
-  if not vim.api.nvim_win_is_valid(state.floating.win) then
-    state.floating = create_floating_window { buf = state.floating.buf }
-    if vim.bo[state.floating.buf].buftype ~= "terminal" then
-      vim.cmd.terminal()
-      vim.cmd.startinsert()
-    end
-  else
-    vim.api.nvim_win_hide(state.floating.win)
-  end
-end, {})
-
-
-vim.api.nvim_create_user_command('SaveAll', function()
-  vim.cmd('wa!')
-  vim.notify("All buffers saved.", vim.log.levels.INFO, { title = "Buffers Written" })
-end, {})
-
-
-vim.api.nvim_create_user_command('ReloadConfig', function()
-  vim.cmd('source ~/.config/nvim/init.lua')
-  vim.notify("Configuration reloaded.", vim.log.levels.INFO, { title = "Config Reloaded" })
+vim.api.nvim_create_user_command("Floaterminal", function()
+	if not vim.api.nvim_win_is_valid(state.floating.win) then
+		state.floating = create_floating_window({ buf = state.floating.buf })
+		if vim.bo[state.floating.buf].buftype ~= "terminal" then
+			vim.cmd.terminal()
+			vim.cmd.startinsert()
+		end
+	else
+		vim.api.nvim_win_hide(state.floating.win)
+	end
 end, {})
--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,5 +1,7 @@
 -- Bootstrap lazy.nvim
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
 	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
@@ -13,23 +15,44 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 		os.exit(1)
 	end
 end
+
+
 vim.opt.rtp:prepend(lazypath)
+
 
 require("lazy").setup({
 	spec = {
-    { "tpope/vim-commentary" },
-		{ "kdheepak/lazygit.nvim" },
-		{ "airblade/vim-gitgutter" },
-		{ "nvim-tree/nvim-web-devicons" },
-		{ "nvim-lua/popup.nvim" },
-		{ "MunifTanjim/nui.nvim" },
-		{ "nvim-lua/plenary.nvim" },
-		{ "ray-x/guihua.lua" },
-		{ "MeanderingProgrammer/render-markdown.nvim" },
-		{ "junegunn/goyo.vim" },
-		{ "junegunn/limelight.vim" },
 		{ "preservim/vim-pencil",
+		{ "tpope/vim-commentary" },
+		{ "kdheepak/lazygit.nvim" },
+		{ "junegunn/limelight.vim" },
+		{ "airblade/vim-gitgutter" },
+		{	"nvim-telescope/telescope.nvim",
 			config = function()
+				local actions = require("telescope.actions")
+				require("telescope").setup({
+					pickers = {
+						find_files = { theme = "ivy" },
+						live_grep = { theme = "ivy" },
+						buffers = { theme = "ivy" },
+						lsp_document_symbols = { theme = "dropdown" },
+					},
+					defaults = {
+						mappings = {
+							i = { ["<esc>"] = actions.close },
+							n = { ["<esc>"] = actions.close },
+							i = { ["<C-v>"] = actions.select_vertical },
+						},
+					},
+				})
+			end,
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+      },
+		},
+		{ "MeanderingProgrammer/render-markdown.nvim" },
+      config = function()
 				vim.g["pencil#wrapModeDefault"] = "soft"
 				vim.g["pencil#textwidthDefault"] = 80
 				vim.g["pencil#autoformat"] = 1
@@ -43,45 +66,20 @@ require("lazy").setup({
 					on_attach = function(bufnr)
 						local bm = require("bookmarks")
 						local map = vim.keymap.set
-						map("n", "mm", bm.bookmark_toggle) -- add or remove bookmark at current line
-						map("n", "mi", bm.bookmark_ann) -- add or edit mark annotation at current line
-						map("n", "mc", bm.bookmark_clean) -- clean all marks in local buffer
-						map("n", "mn", bm.bookmark_next) -- jump to next mark in local buffer
-						map("n", "mp", bm.bookmark_prev) -- jump to previous mark in local buffer
-						map("n", "ml", bm.bookmark_list) -- show marked file list in quickfix window
-						map("n", "mx", bm.bookmark_clear_all) -- removes all bookmarks
+						map("n", "mm", bm.bookmark_toggle)
+						map("n", "mi", bm.bookmark_ann)
+						map("n", "mc", bm.bookmark_clean)
+						map("n", "mn", bm.bookmark_next)
+						map("n", "mp", bm.bookmark_prev)
+						map("n", "ml", bm.bookmark_list)
+						map("n", "mx", bm.bookmark_clear_all)
 					end,
 				})
 			end,
 		},
-		{	"zbirenbaum/copilot.lua",
-			opts = { filetypes = { ["*"] = true } },
-			config = function()
-				require("copilot").setup({
-					suggestion = {
-						auto_trigger = true,
-						keymap = {
-							accept_word = false,
-							accept_line = false,
-							accept = "<S-Tab>",
-						},
-					},
-				})
-			end,
-		},
-		{	"projekt0n/github-nvim-theme",
-			config = function()
-				vim.cmd("colorscheme github_dark_default")
-			end,
-		},
-		{	"ray-x/go.nvim",
-			ft = { "go", "gomod" },
-			event = { "CmdlineEnter" },
-			build = ':lua require("go.install").update_all_sync()',
-			config = function()
-				require("go").setup({
-					run_in_floaterm = { enable = true },
-				})
+    { "projekt0n/github-nvim-theme",
+      config = function()
+        vim.cmd("colorscheme github_dark_default")
 			end,
 		},
 		{ "stevearc/oil.nvim",
@@ -114,28 +112,7 @@ require("lazy").setup({
 				})
 			end,
 		},
-		{ "nvim-telescope/telescope.nvim",
-			config = function()
-				local actions = require("telescope.actions")
-				require("telescope").setup({
-					pickers = {
-						find_files = { theme = "ivy" },
-						live_grep = { theme = "ivy" },
-						buffers = { theme = "ivy" },
-						lsp_document_symbols = { theme = "dropdown" },
-					},
-					defaults = {
-						mappings = {
-							i = { ["<esc>"] = actions.close },
-							n = { ["<esc>"] = actions.close },
-							-- map ctrl+v to open in vertical split
-							i = { ["<C-v>"] = actions.select_vertical },
-						},
-					},
-				})
-			end,
-		},
-		{ "folke/noice.nvim",
+		{ "folke/noice.nvim", 
 			config = function()
 				require("noice").setup({
 					presets = {
@@ -150,6 +127,12 @@ require("lazy").setup({
 					},
 				})
 			end,
+      dependencies = {
+        "MunifTanjim/nui.nvim",
+		 "nvim-lua/popup.nvim" ,
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+      },
 		},
 		{ "rcarriga/nvim-notify",
 			config = function()
@@ -157,35 +140,6 @@ require("lazy").setup({
 				require("notify").setup({
 					stages = "static",
 					timeout = 3000,
-				})
-			end,
-		},
-		{ "neovim/nvim-lspconfig",
-			config = function()
-				require("lspconfig").gopls.setup({
-					settings = {
-						gopls = {
-							analyses = {
-								unusedparams = true,
-								shadow = true,
-							},
-							staticcheck = true,
-							gofumpt = true,
-							vulncheck = true,
-						},
-					},
-				})
-				require("lspconfig").solargraph.setup({
-					cmd = { "solargraph", "stdio" },
-					filetypes = { "ruby" },
-					root_dir = require("lspconfig.util").root_pattern("Gemfile", ".git"),
-					settings = {
-						solargraph = {
-							diagnostics = true,
-							completion = true,
-							formatting = true,
-						},
-					},
 				})
 			end,
 		},
@@ -222,57 +176,79 @@ require("lazy").setup({
 				})
 			end,
 		},
-    { "nvim-treesitter/nvim-treesitter-textobjects",
+		{ "nvim-treesitter/nvim-treesitter-textobjects",
+			config = function()
+				require("nvim-treesitter.configs").setup({
+					textobjects = {
+						select = {
+							enable = true,
+							lookahead = true,
+							keymaps = {
+								["af"] = "@function.outer",
+								["if"] = "@function.inner",
+								["ac"] = "@class.outer",
+								["ic"] = "@class.inner",
+								["as"] = "@scope",
+								["a/"] = "@comment.outer",
+							},
+						},
+					},
+				})
+			end,
+		},
+    {
+			"neovim/nvim-lspconfig",
       config = function()
-        require("nvim-treesitter.configs").setup({
-          textobjects = {
-            select = {
-              enable = true,
-              lookahead = true,
-              keymaps = {
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                ["ic"] = "@class.inner",
-                ["as"] = "@scope",
-                ["a/"] = "@comment.outer",
-              },
-            },
+				require("lspconfig").gopls.setup({
+					settings = {
+						gopls = {
+							analyses = {
+								unusedparams = true,
+								shadow = true,
+							},
+							staticcheck = true,
+							gofumpt = true,
+						},
+					},
+				})
+			end,
+		},
+		{ "ray-x/go.nvim",
+			ft = { "go", "gomod" },
+			event = { "CmdlineEnter" },
+			build = ':lua require("go.install").update_all_sync()',
+			config = function()
+				require("go").setup({
+					run_in_floaterm = { enable = true },
+				})
+			end,
+      dependencies = {
+        "ray-x/guihua.lua",
+        "neovim/nvim-lspconfig",
+        "nvim-treesitter/nvim-treesitter",
+      },
+		},
+    {	"zbirenbaum/copilot.lua",
+    opts = { filetypes = { ["*"] = true } },
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          auto_trigger = true,
+          keymap = {
+            accept_word = false,
+            accept_line = false,
+            accept = "<S-Tab>",
           },
-        })
-      end,
+        },
+      })
+    end,
     },
 
-		{ "yetone/avante.nvim",
-			event = "VeryLazy",
-			version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
-			opts = {
-				provider = "openai",
-				openai = {
-					endpoint = "https://api.openai.com/v1",
-					model = "gpt-4o",
-					timeout = 30000,
-					temperature = 0,
-					max_tokens = 4096,
-				},
-			},
-			build = "make BUILD_FROM_SOURCE=true",
-			dependencies = {
-				"nvim-treesitter/nvim-treesitter",
-				"stevearc/dressing.nvim",
-				"nvim-lua/plenary.nvim",
-				"MunifTanjim/nui.nvim",
-				"echasnovski/mini.pick",
-				"nvim-telescope/telescope.nvim",
-				"hrsh7th/nvim-cmp",
-				"ibhagwan/fzf-lua",
-				"nvim-tree/nvim-web-devicons",
-				"zbirenbaum/copilot.lua",
-				{ "MeanderingProgrammer/render-markdown.nvim",
-					opts = { file_types = { "markdown", "Avante" }, },
-					ft = { "markdown", "Avante" },
-				},
-			},
-		},
-	},
+
+
+
+
+
+  },
 })
+
