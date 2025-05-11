b2ebb2af58801e8759f2e8f611f3a0a7ed39af1d
Seth Miller <seth@sethmiller.me>
2025-05-11 11:16:36 -0500

    update from boron

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -16,11 +16,13 @@ vim.opt.iskeyword:append("-", "_", "@") -- treat dash, underscore and @ as part 
 vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
 vim.opt.wrap = false -- display lines as one long line
 vim.g.mapleader = " " -- set the leader key to space
-vim.cmd("highlight WinSeparator guifg=#A678D3") -- set the color of the window separator
--- vim.o.guifont = "Source Code Pro:h18"                                           -- set the font for neovide
+vim.o.guifont = "Source Code Pro:h14" 
 
 require("plugins")
 
+vim.g.neovide_scale_factor = 0.90
+
+vim.cmd("highlight WinSeparator guifg=#A678D3") -- set the color of the window separator
 local telescope = require("telescope.builtin")
 local opts = { noremap = true, silent = true }
 
@@ -54,23 +56,23 @@ vim.api.nvim_create_user_command("ReloadConfig", function()
 	vim.notify("Configuration reloaded.", vim.log.levels.INFO, { title = "Config Reloaded" })
 end, {})
 
--- vim.api.nvim_create_autocmd("BufWritePre", {
---   pattern = "*.go",
---   callback = function()
---     local params = vim.lsp.util.make_range_params()
---     params.context = {only = {"source.organizeImports"}}
---     local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
---     for cid, res in pairs(result or {}) do
---       for _, r in pairs(res.result or {}) do
---         if r.edit then
---           local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
---           vim.lsp.util.apply_workspace_edit(r.edit, enc)
---         end
---       end
---     end
---     vim.lsp.buf.format({async = false})
---   end
--- })
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    local params = vim.lsp.util.make_range_params()
+    params.context = {only = {"source.organizeImports"}}
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+        end
+      end
+    end
+    vim.lsp.buf.format({async = false})
+  end
+})
 
 local state = {
 	floating = {
@@ -137,3 +139,20 @@ vim.api.nvim_create_user_command("Floaterminal", function()
 		vim.api.nvim_win_hide(state.floating.win)
 	end
 end, {})
+
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+    if client.name == "gopls" then
+      vim.keymap.set("n", "<leader>r", ":lua vim.lsp.buf.rename()<CR>", { buffer = args.buf })
+      vim.keymap.set("n", "<leader>a", ":lua vim.lsp.buf.code_action()<CR>", { buffer = args.buf })
+    end
+
+    if client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true } )
+    end
+
+  end
+})
