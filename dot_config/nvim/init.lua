ec768d52763c19e724b73a3ae5e3be30591ddd9b
Seth Miller <seth@sethmiller.me>
2025-09-10 18:24:01 -0500

    update vim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -150,9 +150,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
       vim.keymap.set("n", "<leader>a", ":lua vim.lsp.buf.code_action()<CR>", { buffer = args.buf })
     end
 
-    if client:supports_method('textDocument/completion') then
-      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true } )
-    end
+    -- if client:supports_method('textDocument/completion') then
+    --   vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true } )
+    -- end
 
   end
 })
