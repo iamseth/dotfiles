ed3c6edd5ca96cba61871a969c8bac25b9f65d4c
Seth Miller <seth@sethmiller.me>
2025-11-05 15:48:37 -0600

    wezterm font

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.font_size = 12.0
+config.font_size = 11.0
 config.tab_bar_at_bottom = false
 config.colors = { foreground = "#FFFFFF" }
 config.hide_tab_bar_if_only_one_tab = true
