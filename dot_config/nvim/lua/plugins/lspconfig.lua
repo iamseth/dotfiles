9b3c9157250ff3bf5ef0f9e6e3f3cfe4da72e88d
Seth Miller <seth@sethmiller.me>
2026-02-24 15:39:38 -0600

    fix blink

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -7,14 +7,17 @@ return {
 			-- Mason must be loaded before its dependents so we need to set it up here.
 			-- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
 			{ "mason-org/mason.nvim", opts = {} },
+
 			"mason-org/mason-lspconfig.nvim",
+
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
 
 			-- Useful status updates for LSP.
 			{ "j-hui/fidget.nvim", opts = {} },
 
 			-- Allows extra capabilities provided by blink.cmp
-			"saghen/blink.cmp",
+			{ "saghen/blink.cmp", version = '*' },
+
 		},
 
 		config = function()
