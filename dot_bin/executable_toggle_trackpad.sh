d6b63e93c1941dc3ed8cc243a93fbc81c2e01cde
Seth Miller <seth@sethmiller.me>
2026-02-24 18:38:27 -0600

    fix trackpad toggle

--- dot_bin/executable_toggle_trackpad.sh
+++ dot_bin/executable_toggle_trackpad.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
+set -euo pipefail
+
+readonly TRACKPAD_ID='i2c-GXTP5100:00'
 
 
-if [ -L "/sys/bus/i2c/drivers/i2c_hid_acpi/i2c-SYNA802E:00" ]; then
+if [ -L "/sys/bus/i2c/drivers/i2c_hid_acpi/${TRACKPAD_ID}" ]; then
   echo "Disabling trackpad"
-  echo "i2c-SYNA802E:00" | sudo tee /sys/bus/i2c/drivers/i2c_hid_acpi/unbind
+  echo "${TRACKPAD_ID}" | sudo tee /sys/bus/i2c/drivers/i2c_hid_acpi/unbind
 else
   echo "Enabling trackpad"
-  echo "i2c-SYNA802E:00" | sudo tee /sys/bus/i2c/drivers/i2c_hid_acpi/bind
+  echo "${TRACKPAD_ID}" | sudo tee /sys/bus/i2c/drivers/i2c_hid_acpi/bind
 fi
-
-
-
-
-
-
-#echo "i2c-SYNA802E:00" | sudo tee /sys/bus/i2c/drivers/i2c_hid_acpi/bind
